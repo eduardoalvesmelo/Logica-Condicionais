0c4559d9fef5cde23dabd8e77a5acbccda106219
Eduardo Alves <eduardoalvesmelo.2018@gmail.com>
2023-12-25 17:33:58 -0400

    Problema "temperatura"

---
+++ src/temperatura.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class temperatura {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Voce vai digitar a temperatura em qual escala (C/F)? ");
+        char c = sc.next().charAt(0);
+
+        if (c == 'F'){
+            System.out.print("Digite a temperatura em Fahrenheit: ");
+            double fahr = sc.nextDouble();
+            double celsius = (fahr - 32) * 5/9;
+            System.out.printf("Temperatura equivalente em Celsius: %.2f", celsius);
+        } else {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double cels = sc.nextDouble();
+            double fahr = (cels * 9/5) + 32;
+            System.out.printf("Temperatura equivalente em Fahrenheit: %.2f", fahr);
+        }
+    }
+}
