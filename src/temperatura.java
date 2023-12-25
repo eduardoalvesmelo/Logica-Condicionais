bf48de449716e9cc2a316d971d6e11c5368e2463
Eduardo Alves <eduardoalvesmelo.2018@gmail.com>
2023-12-25 17:39:28 -0400

    Problema "temperatura"

--- src/temperatura.java
+++ src/temperatura.java
@@ -15,11 +15,13 @@ public class temperatura {
             double fahr = sc.nextDouble();
             double celsius = (fahr - 32) * 5/9;
             System.out.printf("Temperatura equivalente em Celsius: %.2f", celsius);
-        } else {
+        } else if (c == 'C'){
             System.out.print("Digite a temperatura em Celsius: ");
             double cels = sc.nextDouble();
             double fahr = (cels * 9/5) + 32;
             System.out.printf("Temperatura equivalente em Fahrenheit: %.2f", fahr);
+        } else {
+            System.out.printf("Opção não é válida");
         }
     }
 }
